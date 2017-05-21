aac92eb6d3fec1adc10ff096092bf7ed0d48c627
Zhuhao Wang <zhuhaow@gmail.com>
2017-05-21 18:17:08 +0800

    Update mock class

--- test/mock/stream_coder.h
+++ test/mock/stream_coder.h
@@ -47,9 +47,9 @@ class MockStreamCoder : public StreamCoderInterface {
   virtual ~MockStreamCoder() { Die(); }
 
   MOCK_METHOD0(Negotiate, ActionRequest());
-  MOCK_CONST_METHOD0(InputReserve, BufferReserveSize());
+  MOCK_CONST_METHOD0(InputReserve, utils::BufferReserveSize());
   MOCK_METHOD1(Input, ActionRequest(utils::Buffer* buffer));
-  MOCK_CONST_METHOD0(OutputReserve, BufferReserveSize());
+  MOCK_CONST_METHOD0(OutputReserve, utils::BufferReserveSize());
   MOCK_METHOD1(Output, ActionRequest(utils::Buffer* buffer));
   MOCK_CONST_METHOD0(GetLatestError, utils::Error());
   MOCK_CONST_METHOD0(forwarding, bool());
