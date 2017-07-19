21d23d3f513cfcfcb6ee11db81ee4a018c6e24fe
Zhuhao Wang <zhuhaow@gmail.com>
2017-07-19 23:36:17 +0800

    STYLE: Reformat building script according to PEP8

--- scripts/build_deps.py
+++ scripts/build_deps.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
 
-LIBRARIES=[
-    ('muflihun/easyloggingpp', 'v9.94.2', 'easyloggingpp'),
-    ('google/googletest', 'release-1.8.0', 'googletest')
-]
-
-import os, tempfile, shutil, argparse, platform, errno, zipfile
+import os
+import tempfile
+import shutil
+import argparse
+import platform
+import errno
 
 from plumbum import FG, local
 from plumbum.cmd import git, cmake, tar
 
+LIBRARIES = [('muflihun/easyloggingpp', 'v9.94.2', 'easyloggingpp'),
+             ('google/googletest', 'release-1.8.0', 'googletest')]
+
 source_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 install_dir = os.path.abspath(os.path.join(source_dir, "deps"))
+
 
 def ensure_path_exist(path):
     try:
@@ -19,6 +23,7 @@ def ensure_path_exist(path):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+
 
 class Platform:
     iOS = "ios"
@@ -53,7 +58,9 @@ def check_platform(target_platform):
     current_platform = Platform.current_platform()
 
     if current_platform == Platform.OSX:
-        return target_platform in [Platform.OSX, Platform.iOS, Platform.Android]
+        return target_platform in [
+            Platform.OSX, Platform.iOS, Platform.Android
+        ]
     elif current_platform == Platform.Linux:
         return target_platform in [Platform.Linux, Platform.Android]
     elif current_platform == Platform.Windows:
@@ -61,35 +68,44 @@ def check_platform(target_platform):
     else:
         raise Exception("Compiling platform is unknown.")
 
+
 def toolchain_path(target_platform):
-    return os.path.abspath(os.path.join(source_dir, "cmake/toolchain/{}.cmake".format(target_platform)))
+    return os.path.abspath(
+        os.path.join(source_dir, "cmake/toolchain/{}.cmake".format(
+            target_platform)))
+
 
 def install_path(target_platform):
     return os.path.join(install_dir, target_platform)
 
+
 def download_repo(path, url, name, tag):
-    git['-c', 'advice.detachedHead=false',
-        'clone', url,
-        '--branch', tag,
-        '--depth', '1',
-        '--single-branch',
+    git['-c', 'advice.detachedHead=false', 'clone', url, '--branch', tag,
+        '--depth', '1', '--single-branch',
         os.path.join(path, name)] & FG
 
-def cmake_compile(source_dir, install_prefix, target_platform, extra_config=None):
+
+def cmake_compile(source_dir,
+                  install_prefix,
+                  target_platform,
+                  extra_config=None):
     temp_dir = tempfile.mkdtemp()
 
-    config = ["-H{}".format(source_dir),
-              "-B{}".format(temp_dir),
-              "-DCMAKE_BUILD_TYPE=Release",
-              "-DCMAKE_INSTALL_PREFIX={}".format(install_prefix)]
+    config = [
+        "-H{}".format(source_dir), "-B{}".format(temp_dir),
+        "-DCMAKE_BUILD_TYPE=Release",
+        "-DCMAKE_INSTALL_PREFIX={}".format(install_prefix)
+    ]
 
     if target_platform in [Platform.iOS, Platform.OSX]:
-        config.extend(["-GXcode",
-                       "-DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO",
-                       "-DCMAKE_IOS_INSTALL_COMBINED=YES",
-                       "-DIOS_DEPLOYMENT_SDK_VERSION=9.0"])
+        config.extend([
+            "-GXcode", "-DCMAKE_XCODE_ATTRIBUTE_ONLY_ACTIVE_ARCH=NO",
+            "-DCMAKE_IOS_INSTALL_COMBINED=YES",
+            "-DIOS_DEPLOYMENT_SDK_VERSION=9.0"
+        ])
 
-    config.append("-DCMAKE_TOOLCHAIN_FILE={}".format(toolchain_path(target_platform)))
+    config.append(
+        "-DCMAKE_TOOLCHAIN_FILE={}".format(toolchain_path(target_platform)))
 
     if extra_config:
         config.append(extra_config)
@@ -99,21 +115,22 @@ def cmake_compile(source_dir, install_prefix, target_platform, extra_config=None
     if target_platform in [Platform.iOS, Platform.OSX]:
         from plumbum.cmd import xcodebuild
         with local.cwd(os.path.join(temp_dir)):
-            xcodebuild["-target", "install",
-                       "-configuration", "Release"] & FG
+            xcodebuild["-target", "install", "-configuration", "Release"] & FG
     else:
-        cmake["--build",
-              temp_dir,
-              "--", "install"] & FG
+        cmake["--build", temp_dir, "--", "install"] & FG
 
     shutil.rmtree(temp_dir, True)
+
 
 def build_boost(boost_dir, install_prefix, target_platform):
     boost_module = "system"
 
     if target_platform == Platform.iOS:
         # The script will convert the space delimiter back to comma.
-        with local.env(BOOST_SRC=boost_dir, BOOST_LIBS=boost_module.replace(",", " "), OUTPUT_DIR=install_path(target_platform)):
+        with local.env(
+                BOOST_SRC=boost_dir,
+                BOOST_LIBS=boost_module.replace(",", " "),
+                OUTPUT_DIR=install_path(target_platform)):
             with local.cwd(os.path.join(source_dir, "scripts")):
                 local[local.cwd / "build_boost_ios.sh"] & FG
 
@@ -121,10 +138,9 @@ def build_boost(boost_dir, install_prefix, target_platform):
         with local.cwd(boost_dir):
             temp_dir = tempfile.mkdtemp()
 
-            local[os.path.join(boost_dir, "bootstrap.sh")][
-                  "--prefix={}".format(install_prefix),
-                  "--libdir={}".format(temp_dir),
-                  "--with-libraries={}".format(boost_module)] & FG
+            local[os.path.join(boost_dir, "bootstrap.sh")]["--prefix={}".format(
+                install_prefix), "--libdir={}".format(
+                    temp_dir), "--with-libraries={}".format(boost_module)] & FG
             local[os.path.join(boost_dir, "b2")]["install"] & FG
 
             # Linking all modules into one binary file
@@ -142,18 +158,28 @@ def build_boost(boost_dir, install_prefix, target_platform):
 
             shutil.rmtree(temp_dir, True)
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('platform_name', type=str, nargs='?', default=platform.system(), help="The target build platform. Mac, iOS, Android, Linux and Windows are supported.")
+    parser.add_argument(
+        'platform_name',
+        type=str,
+        nargs='?',
+        default=platform.system(),
+        help="The target build platform. Mac, iOS, \
+        Android, Linux and Windows are supported.")
     args = parser.parse_args()
 
     target_platform = Platform.from_string(args.platform_name)
-    assert check_platform(target_platform), "Can't compile dependency for target platform on current platform."
+    assert check_platform(
+        target_platform
+    ), "Can't compile dependency for target platform on current platform."
 
     tempdir = tempfile.mkdtemp()
     try:
         for library in LIBRARIES:
-            download_repo(tempdir, "https://github.com/" + library[0] + ".git", library[2], library[1])
+            download_repo(tempdir, "https://github.com/" + library[0] + ".git",
+                          library[2], library[1])
 
         # Extract boost source to build temp folder
         with local.cwd(tempdir):
@@ -164,16 +190,24 @@ def main():
         ensure_path_exist(install_path(target_platform))
 
         # Compile boost
-        build_boost(os.path.join(tempdir, "boost"), install_path(target_platform), target_platform)
+        build_boost(
+            os.path.join(tempdir, "boost"),
+            install_path(target_platform), target_platform)
 
         # Compile easylogging++
-        cmake_compile(os.path.join(tempdir, "easyloggingpp"), install_path(target_platform), target_platform, "-Dbuild_static_lib=ON")
+        cmake_compile(
+            os.path.join(tempdir, "easyloggingpp"),
+            install_path(target_platform), target_platform,
+            "-Dbuild_static_lib=ON")
 
         # Compile GoogleTest
-        cmake_compile(os.path.join(tempdir, "googletest"), install_path(target_platform), target_platform)
+        cmake_compile(
+            os.path.join(tempdir, "googletest"),
+            install_path(target_platform), target_platform)
 
     finally:
         shutil.rmtree(tempdir, True)
+
 
 if __name__ == "__main__":
     main()
