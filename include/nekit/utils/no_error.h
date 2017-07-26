c6a6e0fb6d9b7abf9abd3c7691ba498136af9d7a
Zhuhao Wang <zhuhaow@gmail.com>
2017-07-26 21:31:19 +0800

    FEAT: Add project level no error error code

---
+++ include/nekit/utils/no_error.h
@@ -0,0 +1,38 @@
+// MIT License
+
+// Copyright (c) 2017 Zhuhao Wang
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include <system_error>
+
+namespace nekit {
+namespace utils {
+enum class NEKitErrorCode { NoError };
+}
+}  // namespace nekit
+
+namespace std {
+template <>
+struct is_error_code_enum<nekit::utils::NEKitErrorCode> : true_type {};
+
+error_code make_error_code(nekit::utils::NEKitErrorCode);
+}  // namespace std
---
+++ src/utils/no_error.cc
@@ -0,0 +1,45 @@
+// MIT License
+
+// Copyright (c) 2017 Zhuhao Wang
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include <string>
+
+#include "nekit/utils/no_error.h"
+
+namespace {
+class NEKitErrorCategory : public std::error_category {
+  const char* name() const noexcept override { return "NEKit"; }
+
+  std::string message(int error_code) const override {
+    switch (static_cast<nekit::utils::NEKitErrorCode>(error_code)) {
+      case nekit::utils::NEKitErrorCode::NoError:
+        return "no error";
+    }
+  }
+};
+
+NEKitErrorCategory nekitErrorCategory{};
+
+}  // namespace
+
+std::error_code std::make_error_code(nekit::utils::NEKitErrorCode ec) {
+  return {static_cast<int>(ec), nekitErrorCategory};
+}
