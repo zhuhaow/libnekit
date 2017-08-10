8c978bb35e46ef9eb9ecbd1581f09ae15b2af1d0
Zhuhao Wang <zhuhaow@gmail.com>
2017-08-10 17:57:47 +0800

    CI: Set up SonarCloud

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,12 +3,22 @@
 set -euo pipefail
 
 [[ "${COVERAGE:-}" ]] || COVERAGE=OFF
+[[ "${COMPILE_PREFIX:-}" ]] || COMPILE_PREFIX=""
 
 cmake -H. -Bbuild -DPLATFORM=$PLATFORM -DCOVERAGE=$COVERAGE -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain/${PLATFORM}.cmake
-cmake --build build
+
+comm="$COMPILE_PREFIX cmake --build build"
+eval $comm
 
 # Test on iOS is still unsupported.
-if [[ $(perl -e "print lc('$PLATFORM');") != "ios" ]]; then
+if [[ $(perl -e "print lc('$PLATFORM');") != "ios" ]]
+then
    cd build
    ctest --output-on-failure
+   cd ..
+fi
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]
+then
+    sonar-scanner
 fi
