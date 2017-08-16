d2bf1da2ab12112c036f40ff7b952ad4ff9f07c7
Zhuhao Wang <zhuhaow@gmail.com>
2017-08-16 23:11:20 +0800

    FEAT: Add key generator for Shadowsocks

---
+++ src/crypto/key_generator.cc
@@ -0,0 +1,85 @@
+// MIT License
+
+// Copyright (c) 2017 Zhuhao Wang
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "nekit/crypto/key_generator.h"
+
+#include <openssl/evp.h>
+
+namespace nekit {
+namespace crypto {
+void KeyGenerator::ShadowsocksGenerate(const uint8_t *data, size_t data_size,
+                                       uint8_t *key, size_t key_size,
+                                       uint8_t *iv, size_t iv_size) {
+  // The following codes are derived from OpenSSL, but heavily modified.
+
+  EVP_MD_CTX *context = EVP_MD_CTX_new();
+  int round = 0;
+  uint8_t digest_buffer[EVP_MAX_MD_SIZE];
+  unsigned int digest_size = 0;
+  size_t key_remain = key_size;
+  size_t iv_remain = iv_size;
+
+  if (!context) {
+    exit(1);
+  }
+
+  while (iv_remain) {
+    if (!EVP_DigestInit_ex(context, EVP_md5(), nullptr)) {
+      exit(1);
+    }
+
+    if (round++) {
+      if (!EVP_DigestUpdate(context, digest_buffer, digest_size)) {
+        exit(1);
+      }
+    }
+
+    if (!EVP_DigestUpdate(context, data, data_size)) {
+      exit(1);
+    }
+
+    if (!EVP_DigestFinal_ex(context, &(digest_buffer[0]), &digest_size)) {
+      exit(1);
+    }
+
+    auto remain = digest_size;
+    while (remain && key_remain) {
+      if (key) {
+        *key++ = digest_buffer[digest_size - remain];
+      }
+      remain--;
+      key_remain--;
+    }
+
+    while (remain && iv_remain) {
+      if (iv) {
+        *iv++ = digest_buffer[digest_size - remain];
+      }
+      remain--;
+      iv_remain--;
+    }
+  }
+
+  EVP_MD_CTX_free(context);
+}
+}  // namespace crypto
+}  // namespace nekit
