061a6dd3104815719c59aa4fed512be64cb2feab
Zhuhao Wang <zhuhaow@gmail.com>
2017-08-16 21:58:23 +0800

    FEAT: Add interface for stream cipher

---
+++ include/nekit/crypto/stream_cipher_interface.h
@@ -0,0 +1,56 @@
+// MIT License
+
+// Copyright (c) 2017 Zhuhao Wang
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include <memory>
+
+#include <boost/noncopyable.hpp>
+
+namespace nekit {
+namespace crypto {
+
+enum class ErrorCode { NoError, ValidationFailed };
+
+enum class Action { Decryption = 0, Encryption = 1 };
+
+// This class provide support for stream cipher or block cipher in stream mode.
+class StreamCipherInterface: private boost::noncopyable {
+ public:
+  virtual ~StreamCipherInterface() = default;
+
+  virtual void SetKey(const uint8_t *data, bool copy) = 0;
+  virtual void SetIv(const uint8_t *data, bool copy) = 0;
+
+  virtual ErrorCode Process(const uint8_t *input, size_t len,
+                            const uint8_t *input_tag, uint8_t *output,
+                            uint8_t *output_tag) = 0;
+
+  virtual void Reset() = 0;
+
+  virtual size_t key_size() = 0;
+  virtual size_t iv_size() = 0;
+  virtual size_t block_size() = 0;
+  virtual size_t tag_size() = 0;
+};
+}  // namespace crypto
+}  // namespace nekit
