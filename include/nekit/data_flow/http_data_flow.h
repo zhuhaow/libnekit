6c74a8e3a85a35ad7ddd0d022e7b217d320bc060
Zhuhao Wang <zhuhaow@gmail.com>
2018-05-25 16:42:44 +0800

    FEAT: Add support for connect to HTTP server

---
+++ include/nekit/data_flow/http_data_flow.h
@@ -0,0 +1,130 @@
+// MIT License
+
+// Copyright (c) 2018 Zhuhao Wang
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include "../utils/http_message_stream_rewriter.h"
+#include "remote_data_flow_interface.h"
+
+namespace nekit {
+namespace data_flow {
+class HttpHeaderRewriterDelegate;
+
+class HttpDataFlow : public RemoteDataFlowInterface {
+ public:
+  class Credential {
+   public:
+    std::string Encode();
+
+   private:
+    std::string username_;
+    std::string password_;
+  };
+
+  enum class ErrorCode { NoError = 0, InvalidResponse, ConnectError };
+
+  HttpDataFlow(std::shared_ptr<utils::Endpoint> server_endpoint,
+               std::shared_ptr<utils::Session> session,
+               std::unique_ptr<RemoteDataFlowInterface>&& data_flow,
+               std::unique_ptr<Credential> credential);
+
+  ~HttpDataFlow();
+
+  utils::Cancelable Read(std::unique_ptr<utils::Buffer>&& buffer,
+                         DataEventHandler handler) override
+      __attribute__((warn_unused_result));
+
+  utils::Cancelable Write(std::unique_ptr<utils::Buffer>&& buffer,
+                          EventHandler handler) override
+      __attribute__((warn_unused_result));
+
+  utils::Cancelable CloseWrite(EventHandler handler) override
+      __attribute__((warn_unused_result));
+
+  bool IsReadClosed() const override;
+
+  bool IsWriteClosed() const override;
+
+  bool IsWriteClosing() const override;
+
+  bool IsReading() const override;
+
+  bool IsWriting() const override;
+
+  data_flow::State State() const override;
+
+  DataFlowInterface* NextHop() const override;
+
+  DataType FlowDataType() const override;
+
+  std::shared_ptr<utils::Session> Session() const override;
+
+  boost::asio::io_context* io() override;
+
+  utils::Cancelable Connect(EventHandler handler) override
+      __attribute__((warn_unused_result));
+
+  RemoteDataFlowInterface* NextRemoteHop() const override;
+
+  std::shared_ptr<utils::Endpoint> ConnectingTo() override;
+
+ private:
+  friend class HttpHeaderRewriterDelegate;
+
+  void ReadResponse(EventHandler handler);
+
+  bool OnMethod();
+  bool OnUrl();
+  bool OnVersion();
+  bool OnStatus();
+  bool OnHeaderPair();
+  bool OnHeaderComplete();
+  bool OnMessageComplete(size_t buffer_offset, bool upgrade);
+
+  std::shared_ptr<utils::Endpoint> server_endpoint_;
+  std::shared_ptr<utils::Endpoint> target_endpoint_;
+  data_flow::State state_{data_flow::State::Closed};
+  bool reading_{false}, writing_{false}, read_closed_{false},
+      write_closed_{false};
+
+  std::shared_ptr<utils::Session> session_;
+  std::unique_ptr<RemoteDataFlowInterface> data_flow_;
+  std::unique_ptr<Credential> credential_;
+
+  utils::Cancelable connect_cancelable_, connect_action_cancelable_,
+      read_cancelable_, write_cancelable_;
+  utils::HttpMessageStreamRewriter rewriter_;
+
+  bool finish_response_{false}, success_response_{false};
+  size_t header_offset_{0};
+  std::unique_ptr<utils::Buffer> pending_payload_;
+};
+
+std::error_code make_error_code(HttpDataFlow::ErrorCode ec);
+}  // namespace data_flow
+}  // namespace nekit
+
+namespace std {
+template <>
+struct is_error_code_enum<nekit::data_flow::HttpDataFlow::ErrorCode>
+    : true_type {};
+}  // namespace std
---
+++ src/data_flow/http_data_flow.cc
@@ -0,0 +1,383 @@
+// MIT License
+
+// Copyright (c) 2018 Zhuhao Wang
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "nekit/data_flow/http_data_flow.h"
+
+#include <boost/algorithm/string.hpp>
+
+#include "nekit/transport/error_code.h"
+
+namespace nekit {
+namespace data_flow {
+
+class HttpHeaderRewriterDelegate
+    : public utils::HttpMessageStreamRewriterDelegateInterface {
+ public:
+  HttpHeaderRewriterDelegate(HttpDataFlow* flow) : flow_{flow} {}
+
+  bool OnMethod(utils::HttpMessageStreamRewriter* rewriter) override {
+    (void)rewriter;
+    return flow_->OnMethod();
+  }
+
+  bool OnUrl(utils::HttpMessageStreamRewriter* rewriter) override {
+    (void)rewriter;
+    return flow_->OnUrl();
+  }
+
+  bool OnVersion(utils::HttpMessageStreamRewriter* rewriter) override {
+    (void)rewriter;
+    return flow_->OnVersion();
+  }
+
+  bool OnStatus(utils::HttpMessageStreamRewriter* rewriter) override {
+    (void)rewriter;
+    return flow_->OnStatus();
+  }
+
+  bool OnHeaderPair(utils::HttpMessageStreamRewriter* rewriter) override {
+    (void)rewriter;
+    return flow_->OnHeaderPair();
+  }
+
+  bool OnHeaderComplete(utils::HttpMessageStreamRewriter* rewriter) override {
+    (void)rewriter;
+    return flow_->OnHeaderComplete();
+  }
+
+  bool OnMessageComplete(utils::HttpMessageStreamRewriter* rewriter,
+                         size_t buffer_offset, bool upgrade) override {
+    (void)rewriter;
+    return flow_->OnMessageComplete(buffer_offset, upgrade);
+  }
+
+ private:
+  HttpDataFlow* flow_;
+};
+
+HttpDataFlow::HttpDataFlow(std::shared_ptr<utils::Endpoint> server_endpoint,
+                           std::shared_ptr<utils::Session> session,
+                           std::unique_ptr<RemoteDataFlowInterface>&& data_flow,
+                           std::unique_ptr<Credential> credential)
+    : server_endpoint_{server_endpoint},
+      session_{session},
+      data_flow_{std::move(data_flow)},
+      credential_{std::move(credential)},
+      rewriter_{utils::HttpMessageStreamRewriter::Type::Response,
+                std::make_shared<HttpHeaderRewriterDelegate>(this)} {
+    rewriter_.SetSkipBodyInResponse(true);
+}
+
+HttpDataFlow::~HttpDataFlow() {
+  connect_cancelable_.Cancel();
+  read_cancelable_.Cancel();
+  connect_action_cancelable_.Cancel();
+  write_cancelable_.Cancel();
+}
+
+utils::Cancelable HttpDataFlow::Read(std::unique_ptr<utils::Buffer>&& buffer,
+                                     DataEventHandler handler) {
+  BOOST_ASSERT(state_ != data_flow::State::Closed &&
+               state_ != data_flow::State::Establishing);
+  BOOST_ASSERT(!reading_);
+  BOOST_ASSERT(!read_closed_);
+
+  reading_ = true;
+
+  read_cancelable_ = data_flow_->Read(
+      std::move(buffer), [this, handler](std::unique_ptr<utils::Buffer> buffer,
+                                         std::error_code ec) {
+        reading_ = false;
+
+        if (ec) {
+          if (ec == nekit::transport::ErrorCode::EndOfFile) {
+            read_closed_ = true;
+            if (write_closed_ && !writing_) {
+              state_ = data_flow::State::Closed;
+            } else {
+              state_ = data_flow::State::Closing;
+            }
+          } else {
+            read_closed_ = true;
+            write_closed_ = true;
+            write_cancelable_.Cancel();
+            state_ = data_flow::State::Closed;
+          }
+          handler(std::move(buffer), ec);
+          return;
+        }
+
+        handler(std::move(buffer), ec);
+      });
+
+  return read_cancelable_;
+}
+
+utils::Cancelable HttpDataFlow::Write(std::unique_ptr<utils::Buffer>&& buffer,
+                                      EventHandler handler) {
+  BOOST_ASSERT(state_ != data_flow::State::Closed &&
+               state_ != data_flow::State::Establishing);
+  BOOST_ASSERT(!writing_);
+  BOOST_ASSERT(!write_closed_);
+
+  writing_ = true;
+  write_cancelable_ =
+      data_flow_->Write(std::move(buffer), [this, handler](std::error_code ec) {
+        writing_ = false;
+
+        if (ec) {
+          read_closed_ = true;
+          write_closed_ = true;
+          state_ = data_flow::State::Closed;
+          read_cancelable_.Cancel();
+        }
+        handler(ec);
+      });
+  return write_cancelable_;
+}
+
+utils::Cancelable HttpDataFlow::CloseWrite(EventHandler handler) {
+  BOOST_ASSERT(state_ != data_flow::State::Closed &&
+               state_ != data_flow::State::Establishing);
+  BOOST_ASSERT(!writing_);
+  BOOST_ASSERT(!write_closed_);
+
+  writing_ = true;
+  write_closed_ = true;
+
+  state_ = data_flow::State::Closing;
+
+  write_cancelable_ =
+      data_flow_->CloseWrite([this, handler](std::error_code ec) {
+        writing_ = false;
+
+        if (read_closed_) {
+          state_ = data_flow::State::Closed;
+        }
+
+        handler(ec);
+      });
+
+  return write_cancelable_;
+}
+
+bool HttpDataFlow::IsReadClosed() const {
+  BOOST_ASSERT(NE_DATA_FLOW_CAN_CHECK_CLOSE_STATE(state_));
+  return read_closed_;
+}
+
+bool HttpDataFlow::IsWriteClosed() const {
+  BOOST_ASSERT(NE_DATA_FLOW_CAN_CHECK_CLOSE_STATE(state_));
+  return write_closed_;
+}
+
+bool HttpDataFlow::IsWriteClosing() const {
+  BOOST_ASSERT(NE_DATA_FLOW_CAN_CHECK_CLOSE_STATE(state_));
+  return write_closed_ && writing_;
+}
+
+bool HttpDataFlow::IsReading() const {
+  BOOST_ASSERT(NE_DATA_FLOW_CAN_CHECK_DATA_STATE(state_));
+  return reading_;
+}
+
+bool HttpDataFlow::IsWriting() const {
+  BOOST_ASSERT(NE_DATA_FLOW_CAN_CHECK_DATA_STATE(state_));
+  return writing_;
+}
+
+data_flow::State HttpDataFlow::State() const { return state_; }
+
+DataFlowInterface* HttpDataFlow::NextHop() const { return data_flow_.get(); }
+
+DataType HttpDataFlow::FlowDataType() const { return DataType::Stream; }
+
+std::shared_ptr<utils::Session> HttpDataFlow::Session() const {
+  return session_;
+}
+
+boost::asio::io_context* HttpDataFlow::io() { return session_->io(); }
+
+utils::Cancelable HttpDataFlow::Connect(EventHandler handler) {
+  BOOST_ASSERT(state_ == data_flow::State::Closed);
+
+  target_endpoint_ = session_->current_endpoint();
+  session_->set_current_endpoint(server_endpoint_);
+
+  BOOST_ASSERT(target_endpoint_);
+
+  state_ = data_flow::State::Establishing;
+
+  connect_cancelable_ = utils::Cancelable();
+  connect_action_cancelable_ = data_flow_->Connect(
+      [this, handler, cancelable{connect_cancelable_}](std::error_code ec) {
+        if (cancelable.canceled()) {
+          return;
+        }
+
+        if (ec) {
+          state_ = data_flow::State::Closed;
+          handler(ec);
+          return;
+        };
+
+        std::ostringstream os;
+        os << "CONNECT " << target_endpoint_->host() << ":"
+           << target_endpoint_->port() << " HTTP/1.1\r\n";
+        os << "Host: " << target_endpoint_->host() << ":"
+           << target_endpoint_->port() << "\r\n";
+        if (credential_) {
+          os << "Proxy-Authorization: Basic "
+             << "\r\n";
+        }
+        os << "\r\n";
+
+        std::string request = os.str();
+
+        auto buffer = std::make_unique<utils::Buffer>(request.size());
+        buffer->SetData(0, request.size(), request.c_str());
+
+        connect_action_cancelable_ = data_flow_->Write(
+            std::move(buffer),
+            [this, handler, cancelable{cancelable}](std::error_code ec) {
+              if (cancelable.canceled()) {
+                return;
+              }
+
+              if (ec) {
+                state_ = data_flow::State::Closed;
+                handler(ec);
+                return;
+              }
+
+              ReadResponse(handler);
+            });
+      });
+
+  return connect_cancelable_;
+}
+
+RemoteDataFlowInterface* HttpDataFlow::NextRemoteHop() const {
+  return data_flow_.get();
+}
+
+std::shared_ptr<utils::Endpoint> HttpDataFlow::ConnectingTo() {
+  return target_endpoint_;
+}
+
+void HttpDataFlow::ReadResponse(EventHandler handler) {
+  auto buffer = std::make_unique<utils::Buffer>(96);
+  connect_action_cancelable_ = data_flow_->Read(
+      std::move(buffer),
+      [this, handler, cancelable{connect_cancelable_}](
+          std::unique_ptr<utils::Buffer>&& buffer, std::error_code ec) {
+        if (cancelable.canceled()) {
+          return;
+        }
+
+        if (ec) {
+          state_ = State::Closed;
+          if (ec == nekit::transport::ErrorCode::EndOfFile) {
+            handler(ErrorCode::InvalidResponse);
+          } else {
+            handler(ec);
+          }
+          return;
+        }
+
+        if (!rewriter_.RewriteBuffer(buffer.get())) {
+          state_ = State::Closed;
+          handler(ErrorCode::InvalidResponse);
+          return;
+        }
+
+        if (finish_response_) {
+          if (success_response_) {
+            buffer->ShrinkFront(header_offset_);
+            if (buffer->size()) {
+              pending_payload_ = std::move(buffer);
+            }
+
+            state_ = State::Established;
+            handler(ErrorCode::NoError);
+          } else {
+            state_ = State::Closed;
+            handler(ErrorCode::ConnectError);
+          }
+          return;
+        }
+
+        ReadResponse(handler);
+      });
+}
+
+bool HttpDataFlow::OnMethod() { return false; }
+
+bool HttpDataFlow::OnUrl() { return false; }
+
+bool HttpDataFlow::OnVersion() { return true; }
+
+bool HttpDataFlow::OnStatus() {
+  success_response_ = boost::istarts_with(rewriter_.CurrentToken(), "200");
+
+  return true;
+}
+
+bool HttpDataFlow::OnHeaderPair() { return true; }
+
+bool HttpDataFlow::OnHeaderComplete() { return true; }
+
+bool HttpDataFlow::OnMessageComplete(size_t buffer_offset, bool upgrade) {
+  (void)upgrade;
+  header_offset_ = buffer_offset;
+  finish_response_ = true;
+  return true;
+}
+namespace {
+struct HttpDataFlowErrorCategory : std::error_category {
+  const char* name() const noexcept override;
+  std::string message(int) const override;
+};
+
+const char* HttpDataFlowErrorCategory::name() const BOOST_NOEXCEPT {
+  return "HTTP stream coder";
+}
+
+std::string HttpDataFlowErrorCategory::message(int error_code) const {
+  switch (static_cast<HttpDataFlow::ErrorCode>(error_code)) {
+    case HttpDataFlow::ErrorCode::NoError:
+      return "no error";
+    case HttpDataFlow::ErrorCode::InvalidResponse:
+      return "server send an invalid response";
+    case HttpDataFlow::ErrorCode::ConnectError:
+      return "connect failed since the server returns a non 200 response";
+  }
+}
+
+const HttpDataFlowErrorCategory httpDataFlowErrorCategory{};
+}  // namespace
+
+std::error_code make_error_code(HttpDataFlow::ErrorCode ec) {
+  return {static_cast<int>(ec), httpDataFlowErrorCategory};
+}
+}  // namespace data_flow
+}  // namespace nekit
