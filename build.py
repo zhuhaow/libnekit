5d2f585779bef5e8bd82e7f4e7b46818153af711
zhuhaow <zhuhaow@gmail.com>
2018-09-18 14:49:05 +0800

    CI: Use std 14 in CI

--- build.py
+++ build.py
@@ -3,4 +3,9 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(pure_c=False)
+    builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        settings["cppstd"] = 14
+        builds.append([settings, options, env_vars, build_requires])
+    builder.builds = builds
     builder.run()
