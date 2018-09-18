8bd2c44e9a2123aad8c86959f7f76283af95d3f2
zhuhaow <zhuhaow@gmail.com>
2018-09-18 13:25:52 +0800

    CI: Test with conan

--- scripts/install_cmake.sh
+++ scripts/install_cmake.sh
@@ -2,9 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-curl https://cmake.org/files/v3.9/cmake-3.9.0-Linux-x86_64.sh -o $DIR/../cmake.sh
+curl https://cmake.org/files/v3.12/cmake-3.12.2-Linux-x86_64.sh -o $DIR/../../cmake.sh
 
-mkdir -p $DIR/../deps/cmake
+mkdir -p $DIR/../../cmake
 
-sh $DIR/../cmake.sh --prefix=$DIR/../deps/cmake --skip-license
-
+sh $DIR/../../cmake.sh --prefix=$DIR/../../cmake --skip-license
