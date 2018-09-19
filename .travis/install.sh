4e9410341c913d680ed3f791c1caffdcbd0be52e
zhuhaow <zhuhaow@gmail.com>
2018-09-19 11:00:45 +0800

    CI: Cache pyenv installed python

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,7 +13,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 2.7.10
+    pyenv install -s 2.7.10
     pyenv virtualenv 2.7.10 conan
     pyenv rehash
     pyenv activate conan
